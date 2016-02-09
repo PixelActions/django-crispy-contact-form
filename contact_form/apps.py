0fed1d7689e07cb0d14a36f6d2203bf40afa3f8a
Kyriakos Toumbas <kyriakos@pixelactions.com>
2016-02-09 03:51:03 +0200

    added missing settings import

--- contact_form/apps.py
+++ contact_form/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig as DjangoAppConfig
 from django.utils.translation import ugettext_lazy as _
 from contact_form.conf import settings
+from django.dispatch import receiver
+from django.core.mail import EmailMessage
 
 class AppConfig(DjangoAppConfig):
     """Configuration for the contact_form app (only for Django v1.7+)"""
--- contact_form/signals.py
+++ contact_form/signals.py
@@ -1,7 +1,4 @@
 from django.dispatch import Signal
-from django.dispatch import receiver
-from contact_form.conf import settings
-from django.core.mail import EmailMessage
 
 contact_form_valid = Signal(
     providing_args=['event', 'ip', 'site', 'sender_name', 'sender_email', 'email', 'subject', 'message']
