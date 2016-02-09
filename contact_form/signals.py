04f76eea1d403a7de22881667d7589d9e85eef69
Kyriakos Toumbas <kyriakos@pixelactions.com>
2016-02-09 04:15:23 +0200

    added missing settings import

--- contact_form/signals.py
+++ contact_form/signals.py
@@ -1,4 +1,7 @@
 from django.dispatch import Signal
+from contact_form.conf import settings
+from django.dispatch import receiver
+from django.core.mail import EmailMessage
 
 contact_form_valid = Signal(
     providing_args=['event', 'ip', 'site', 'sender_name', 'sender_email', 'email', 'subject', 'message']
@@ -7,3 +10,18 @@ contact_form_valid = Signal(
 contact_form_invalid = Signal(
     providing_args=['event', 'ip', 'site', 'sender_name', 'sender_email']
 )
+
+
+@receiver(contact_form_valid)
+def contact_form_send_email_on_valid(sender, event, ip, site, sender_name, sender_email, email, subject, message, **kwargs):
+    if settings.CONTACT_FORM_SEND_EMAIL_ON_VALID:
+        if settings.CONTACT_FORM_SEND_BBC_TO:
+            bcc = [settings.CONTACT_FORM_SEND_BBC_TO,]
+        else:
+            bcc = []
+        email = EmailMessage(subject="%s: %s" %(sender_name,subject), body=message,#from_email=django_settings.DEFAULT_FROM_EMAIL  we ommit cause it happens automaticlaly
+        to=[email,], bcc=bcc , reply_to=[sender_email], headers={})
+        email.send(fail_silently=False)
+
+        
+
