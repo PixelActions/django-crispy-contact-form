fef334930932e882ee2e3f3c6a2f82336d632536
Kyriakos Toumbas <kyriakos@pixelactions.com>
2016-02-09 03:47:56 +0200

    moved signalo registration to appconfig

--- contact_form/apps.py
+++ contact_form/apps.py
@@ -6,3 +6,20 @@ class AppConfig(DjangoAppConfig):
     """Configuration for the contact_form app (only for Django v1.7+)"""
     label = name = 'contact_form'
     verbose_name = _('Contact Form')
+
+    def ready(self):
+        if settings.CONTACT_FORM_SEND_EMAIL_ON_VALID:
+            from .signals import contact_form_valid, contact_form_invalid
+            
+            @receiver(contact_form_valid)
+            def contact_form_send_email_on_valid(sender, event, ip, site, sender_name, sender_email, email, subject, message, **kwargs):
+                if settings.SEND_BBC_TO:
+                    bcc = [settings.SEND_BBC_TO,]
+                else:
+                    bcc = []
+                email = EmailMessage(subject="%s: %s" %(sender_name,subject), body=message,#from_email=django_settings.DEFAULT_FROM_EMAIL  we ommit cause it happens automaticlaly
+                to=[email,], bcc=bcc , reply_to=[sender_email], headers={})
+                email.send(fail_silently=False)
+
+        
+
--- contact_form/signals.py
+++ contact_form/signals.py
@@ -10,14 +10,3 @@ contact_form_valid = Signal(
 contact_form_invalid = Signal(
     providing_args=['event', 'ip', 'site', 'sender_name', 'sender_email']
 )
-
-if settings.CONTACT_FORM_SEND_EMAIL_ON_VALID:
-    @receiver(contact_form_valid)
-    def contact_form_send_email_on_valid(sender, event, ip, site, sender_name, sender_email, email, subject, message, **kwargs):
-        if settings.SEND_BBC_TO:
-            bcc = [settings.SEND_BBC_TO,]
-        else:
-            bcc = []
-        email = EmailMessage(subject="%s: %s" %(sender_name,subject), body=message,#from_email=django_settings.DEFAULT_FROM_EMAIL  we ommit cause it happens automaticlaly
-        to=[email,], bcc=bcc , reply_to=[sender_email], headers={})
-        email.send(fail_silently=False)
